2000a236870387811096d1efdd515d670ef30f39
Facundo Prato <facuigna2005@gmail.com>
2024-07-11 08:39:56 -0300

    punto 2

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost',12345))
+s.bind(('127.0.0.1',12345))
 s.listen()
 conn, addr = s.accept()
 data = conn.recv(1024)
 conn.sendall('Message for client'.encode())
+print(data)
