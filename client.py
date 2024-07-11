abfcf434c1b673662a2df2382777f5dc2a753d13
Facundo Prato <facuigna2005@gmail.com>
2024-07-11 09:02:46 -0300

    punto 5

--- client.py
+++ client.py
@@ -1,11 +1,6 @@
 import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-s.sendto("Hola, servidor".encode(), ('127.0.0.1', 91218))
-
-response, addr = s.recvfrom(1024)
-response = response.decode()
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 91218))
+s.send("Hola, servidor".encode())
+response = s.recv(1024).decode()
 print(response)
-
-s.close()
--- server.py
+++ server.py
@@ -1,22 +1,8 @@
 import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-try:
-    s.bind(('127.0.0.1', 91218))
-
-    print("UDP Server esta escuchando 127.0.0.1:")
-
-    while True:
-        data, addr = s.recvfrom(1024)
-        if data:
-            print(f"recibido de {addr}: {data.decode()}")
-
-            message = 'mensaje para el cliente'
-            s.sendto(message.encode(), addr)
-
-except socket.error as e:
-    print(f"Socket error: {e}")
-
-finally:
-    s.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1',91218))
+s.listen()
+conn, addr = s.accept()
+data = conn.recv(1024)
+conn.sendall('Message for client'.encode())
+print(data)
