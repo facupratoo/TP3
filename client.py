bfa949616ac00204753584e8179ce6008fb4b186
Facundo Prato <facuigna2005@gmail.com>
2024-07-11 08:47:43 -0300

    punto 3

--- client.py
+++ client.py
@@ -1,6 +1,11 @@
 import socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 12345))
-s.send("Hola, servidor".encode())
-response = s.recv(1024).decode()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.sendto("Hola, servidor".encode(), ('127.0.0.1', 12345))
+
+response, addr = s.recvfrom(1024)
+response = response.decode()
 print(response)
+
+s.close()
--- server.py
+++ server.py
@@ -1,8 +1,22 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1',12345))
-s.listen()
-conn, addr = s.accept()
-data = conn.recv(1024)
-conn.sendall('Message for client'.encode())
-print(data)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    s.bind(('127.0.0.1', 12345))
+
+    print("UDP Server esta escuchando 127.0.0.1:12345")
+
+    while True:
+        data, addr = s.recvfrom(1024)
+        if data:
+            print(f"Received from {addr}: {data.decode()}")
+
+            message = 'mensaje para el cliente'
+            s.sendto(message.encode(), addr)
+
+except socket.error as e:
+    print(f"Socket error: {e}")
+
+finally:
+    s.close()
