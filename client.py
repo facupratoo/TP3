7860f2997be2fd64dc22efd662be3381e9d497b5
Facundo Prato <facuigna2005@gmail.com>
2024-07-11 09:01:40 -0300

    punto 4

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s.sendto("Hola, servidor".encode(), ('127.0.0.1', 12345))
+s.sendto("Hola, servidor".encode(), ('127.0.0.1', 91218))
 
 response, addr = s.recvfrom(1024)
 response = response.decode()
--- server.py
+++ server.py
@@ -3,14 +3,14 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
-    s.bind(('127.0.0.1', 12345))
+    s.bind(('127.0.0.1', 91218))
 
-    print("UDP Server esta escuchando 127.0.0.1:12345")
+    print("UDP Server esta escuchando 127.0.0.1:")
 
     while True:
         data, addr = s.recvfrom(1024)
         if data:
-            print(f"Received from {addr}: {data.decode()}")
+            print(f"recibido de {addr}: {data.decode()}")
 
             message = 'mensaje para el cliente'
             s.sendto(message.encode(), addr)
